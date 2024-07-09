0a1dfc5096ffe56eba24517d6d4fa3b098edfa3c
Junaid Saleem <junaidsaleem986@gmail.com>
2024-07-09 17:58:48 +0500

    user profile

--- base/views.py
+++ base/views.py
@@ -14,16 +14,29 @@ def home(request):
     topics = Topic.objects.all()
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(description__icontains=q) | Q(name__icontains=q))
     room_count = rooms.count()
+    room_messages = Message.objects.all().filter(Q(room__topic__name__icontains = q))
     context = {
         "rooms":rooms,
         "topics":topics,
         "room_count":room_count,
+        "room_messages":room_messages,
         }
     return render(request,'base/home.html',context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {"room":room}
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+    if request.method=='POST':
+        message = Message.objects.create(
+            user = request.user, room = room, body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
+    context = {"room":room,
+               "room_messages":room_messages,
+               "participants":participants,
+               }
     return render(request,'base/room.html',context)
 
 @login_required(login_url='login')
@@ -32,7 +45,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {
@@ -68,6 +83,19 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     context = {'obj':room}
+    return render(request,'base/delete.html',context)
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if request.user!=message.user:
+        messages.error(request,'Access Denied')
+        return redirect('room')
+
+    if request.method=='POST':
+        message.delete()
+        return redirect('home')
+    context = {'obj':message}
     return render(request,'base/delete.html',context)
     
 def loginPage(request):
@@ -114,3 +142,20 @@ def logoutPage(request):
     logout(request)
     messages.info(request,"Successfully Logged Out")
     return redirect('home')
+
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    if user != request.user:
+        messages.error(request,'Access Denied')
+        return redirect('home')
+    
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {
+        'user':user,
+        'rooms':rooms,
+        'room_messages':room_messages,
+        'topics':topics,
+    }
+    return render(request,'base/profile.html',context)
