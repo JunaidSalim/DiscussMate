05bcc7bfd8a6ad280eb9a701e67ce3dbb35f806d
Junaid Saleem <junaidsaleem986@gmail.com>
2024-07-14 13:06:01 +0500

    database

--- mate/settings.py
+++ mate/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from environ import Env
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,6 +82,8 @@ DATABASES = {
     }
 }
 
+if ENVIRONMENT == 'production':
+    DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
